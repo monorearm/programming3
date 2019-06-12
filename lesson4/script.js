1ba0ad5268ca2bf785a369ca7ddac4b9e3dcfe6b
yourusername <your@email.com>
2019-06-12 23:44:19 +0400

    Avelacri jnjelu funkcian

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,25 +1,42 @@
 function main() {
-   var socket = io();
-   var chatDiv = document.getElementById('chat');
-   var input = document.getElementById('message');
-   var button = document.getElementById('submit');
+    var socket = io();
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+    var del = document.getElementById("del")
 
-   function handleSubmit(evt) {
-       var val = input.value;
-       if (val != "") {
-           socket.emit("send message", val);
-       }
-   }
-   button.onclick = handleSubmit;
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
 
-   function handleMessage(msg) {
-    var p = document.createElement('p');
-    p.innerText = msg;
-    chatDiv.appendChild(p);
-    input.value = "";
-}
+    function handleDelete() {
+        socket.emit("uzum em jnjem");
+    }
 
-socket.on('display message', handleMessage);
+    button.onclick = handleSubmit;
+    
+    del.onclick = handleDelete;
+
+    function handleMessage(msg) {
+        var p = document.createElement('p');
+        p.innerText = msg;
+        chatDiv.appendChild(p);
+        input.value = "";
+    }
+
+    function deleteFromDom() {
+        var p = document.getElementsByTagName("p");
+        for (var i in p) {
+            if (p.length > 0) {
+                chatDiv.removeChild(p[0]);
+            }
+        }
+    }
+    socket.on('display message', handleMessage);
+    socket.on('de dzer motic jnjeq', deleteFromDom);
 } // main closing bracket
 
 window.onload = main;
--- lesson4/server.js
+++ lesson4/server.js
@@ -6,18 +6,21 @@ var messages = [];
 
 app.use(express.static("."));
 app.get('/', function (req, res) {
-   res.redirect('index.html');
+  res.redirect('index.html');
 });
 server.listen(3000);
 
 
 io.on('connection', function (socket) {
-    for(var i in messages) {
-      io.sockets.emit("display message", messages[i]);
-    }
-    socket.on("send message", function (data) {
-        messages.push(data);
-        io.sockets.emit("display message", data);
-    });
- });
- 
+  for (var i in messages) {
+    io.sockets.emit("display message", messages[i]);
+  }
+  socket.on("send message", function (data) {
+    messages.push(data);
+    io.sockets.emit("display message", data);
+  });
+  socket.on("uzum em jnjem", function () {
+    messages = [];
+    io.sockets.emit("de dzer motic jnjeq");
+  });
+});
